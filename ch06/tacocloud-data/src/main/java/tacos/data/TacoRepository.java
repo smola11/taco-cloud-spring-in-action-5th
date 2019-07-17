8144cefe49510c756aff98ee52bb1c213424c106
maciej <smola11@o2.pl>
2019-07-17 13:05:49 +0200

    First change in web api - DesignTacoController

--- ch06/tacocloud-data/src/main/java/tacos/data/TacoRepository.java
+++ ch06/tacocloud-data/src/main/java/tacos/data/TacoRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import tacos.Taco;
 
 
-public interface TacoRepository 
-         extends PagingAndSortingRepository<Taco, Long> {
-
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
 }
