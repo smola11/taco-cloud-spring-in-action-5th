04fba59af879f094653334acc708bdd52fab5bdd
maciej <smola11@o2.pl>
2019-08-04 12:19:47 +0200

    Added rabbitmq sender

---
+++ ch06-07/tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/RabbitOrderMessagingService.java
@@ -0,0 +1,25 @@
+package tacos.messaging;
+
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import tacos.Order;
+
+public class RabbitOrderMessagingService implements OrderMessagingService {
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitOrderMessagingService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void sendOrder(Order order) {
+        rabbitTemplate.convertAndSend("tacocloud.order.queue", order,
+                message -> {
+                    MessageProperties props = message.getMessageProperties();
+                    props.setHeader("X_ORDER_SOURCE", "WEB");
+                    return message;
+                });
+    }
+}
+
