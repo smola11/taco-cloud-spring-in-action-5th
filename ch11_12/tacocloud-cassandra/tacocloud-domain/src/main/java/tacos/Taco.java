93c7518c64a515e6ee5457d9b65a4a488751ef4f
maciej <smola11@o2.pl>
2019-10-14 14:33:13 +0200

    Classes for Cassandra persistence

--- ch11_12/tacocloud-cassandra/tacocloud-domain/src/main/java/tacos/Taco.java
+++ ch11_12/tacocloud-cassandra/tacocloud-domain/src/main/java/tacos/Taco.java
@@ -20,22 +20,31 @@ import lombok.Data;
 
 @Data
 @RestResource(rel = "tacos", path = "tacos")
-@Table("tacos")
+@Table("tacos") // Persist to "tacos" table in Cassandra
 public class Taco {
 
-  @PrimaryKeyColumn(type=PrimaryKeyType.PARTITIONED)
-  private UUID id = UUIDs.timeBased();
-  
-  @NotNull
-  @Size(min = 5, message = "Name must be at least 5 characters long")
-  private String name;
-  
-  @PrimaryKeyColumn(type=PrimaryKeyType.CLUSTERED,
-                    ordering=Ordering.DESCENDING)
-  private Date createdAt = new Date();
-  
-  @Size(min=1, message="You must choose at least 1 ingredient")
-  @Column("ingredients")
-  private List<IngredientUDT> ingredients;
+    // Defines the Partition Key.
+    // This specifies that the 'id' property serves as the partition key, used to determine which Cassandra
+    // partition(s) each row of taco data will be written to.
+    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
+    private UUID id = UUIDs.timeBased();
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    // Defines the Clustering Key.
+    // Clustering keys are used to determine the ordering of rows within a partition.
+    // More specifically, the ordering is set to descending order — therefore, within a given partition, newer rows
+    // appear first in the tacos table.
+    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED,
+        ordering = Ordering.DESCENDING)
+    private Date createdAt = new Date();
+
+    // Columns that contain collections of data, such as the ingredients column, must be collections of native types
+    // (integers, strings, and so on) or must be collections of user-defined types [IngredientUDT].
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @Column("ingredients")
+    private List<IngredientUDT> ingredients;
 
 }
